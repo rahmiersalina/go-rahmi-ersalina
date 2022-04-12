2e0749b30287d6b7cfb5241243e7a1ab0a0d0d2e
rahmiersalina <rahmisenyo1234@gmail.com>
2022-04-12 16:59:30 +0700

    pratikum

---
+++ 06_data structure/pratikum/arraymarge.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	arrA := []string{"justin", "zayn", "nichol", "evan", "jade"}
+	arrB := []string{"kendall", "gigi", "bella", "selena", "hayley"}
+	fmt.Println(ArryMerge(arrA, arrB))
+}
+func ArryMerge(arrA, arrB []string) (combined []string) {
+	hmap := make(map[string]bool)
+	for _, valueA := range arrA {
+		hmap[valueA] = true
+		combined = append(combined, valueA)
+	}
+	for _, valueB := range arrB {
+		_, isfound := hmap[valueB]
+		if isfound == false {
+			hmap[valueB] = true
+			combined = append(combined, valueB)
+		}
+	}
+	return combined
+
+}
